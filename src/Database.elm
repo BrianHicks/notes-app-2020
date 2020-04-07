77a80ad00ff7d035e7f28ff7e391b9ef0ee5a685
Brian Hicks <brian@brianthicks.com>
2020-04-07 06:19:51 -0500

    add editing

--- src/Database.elm
+++ src/Database.elm
@@ -1,4 +1,4 @@
-module Database exposing (Database, init, insert, notes)
+module Database exposing (Database, get, init, insert, notes, update)
 
 import Node exposing (Node)
 import Sort.Dict as Dict exposing (Dict)
@@ -23,3 +23,13 @@ notes (Database database) =
 insert : Node -> Database -> Database
 insert ({ id } as node) (Database database) =
     Database (Dict.insert id node database)
+
+
+get : Node.ID -> Database -> Maybe Node
+get id (Database database) =
+    Dict.get id database
+
+
+update : Node.ID -> (Node -> Node) -> Database -> Database
+update id fn (Database database) =
+    Database (Dict.update id (Maybe.map fn) database)
--- src/Main.elm
+++ src/Main.elm
@@ -2,10 +2,12 @@ module Main exposing (main)
 
 import Browser
 import Browser.Navigation as Navigation
+import Css
 import Database exposing (Database)
 import Html.Styled as Html exposing (Html)
+import Html.Styled.Attributes as Attrs exposing (css)
 import Html.Styled.Events as Events
-import Node
+import Node exposing (Node)
 import Random
 import Task
 import Time exposing (Posix)
@@ -18,6 +20,10 @@ type alias Model =
     , url : Url
     , key : Navigation.Key
     , seed : Random.Seed
+
+    -- view state
+    , selected : Maybe Node.ID
+    , editing : Maybe Node.ID
     }
 
 
@@ -27,6 +33,12 @@ type Msg
     | ClickedNewNote
     | NewNote UUID Posix
     | NewRandomSeed Random.Seed
+    | ClickedToSelectNote Node.ID
+    | ClickedToEditNode Node.ID
+    | StoppedEditingNode
+      -- TODO: better names for this distinction. EditedNodeContents is for editing, EditNodeContents is for getting the timestamp
+    | EditedNodeContents String
+    | UpdatedNote Node.ID Posix
 
 
 init : () -> Url -> Navigation.Key -> ( Model, Cmd Msg )
@@ -35,6 +47,10 @@ init flags url key =
       , url = url
       , key = key
       , seed = Random.initialSeed 0 -- TODO: current time?
+
+      -- view state
+      , selected = Nothing
+      , editing = Nothing
       }
     , Random.generate NewRandomSeed Random.independentSeed
     )
@@ -81,7 +97,43 @@ update msg model =
                         , updated = time
                         }
                         model.database
+                , selected = Just (Node.ID uuid)
+                , editing = Just (Node.ID uuid)
               }
+            , Cmd.none
+            )
+
+        ClickedToSelectNote id ->
+            -- TODO: validate this is a valid ID
+            ( { model | selected = Just id }
+            , Cmd.none
+            )
+
+        ClickedToEditNode id ->
+            -- TODO: validate this is a valid ID
+            ( { model | editing = Just id }
+            , Cmd.none
+            )
+
+        StoppedEditingNode ->
+            ( { model | editing = Nothing }
+            , Cmd.none
+            )
+
+        EditedNodeContents newContent ->
+            case model.editing of
+                Just id ->
+                    ( { model | database = Database.update id (\node -> { node | content = newContent }) model.database }
+                    , Task.perform (UpdatedNote id) Time.now
+                    )
+
+                Nothing ->
+                    ( model
+                    , Cmd.none
+                    )
+
+        UpdatedNote id newTimestamp ->
+            ( { model | database = Database.update id (\node -> { node | updated = newTimestamp }) model.database }
             , Cmd.none
             )
 
@@ -96,17 +148,69 @@ view model =
     { title = "Notes"
     , body =
         [ Html.toUnstyled <|
-            Html.main_ []
-                [ Html.p [] [ Html.text (Debug.toString model) ]
-                , Html.button
-                    [ Events.onClick ClickedNewNote ]
-                    [ Html.text "New Note" ]
-                , Database.notes model.database
-                    |> List.map (\node -> Html.li [] [ Html.text (Debug.toString node) ])
-                    |> Html.ul []
+            Html.main_
+                [ css
+                    [ Css.property "display" "grid"
+                    , Css.property "grid-template-areas" "\"header content\" \"notes content\" \"debug debug\""
+                    , Css.property "grid-template-rows" "100px 1fr 100px"
+                    , Css.property "grid-template-columns" "300px 1fr"
+                    , Css.height (Css.vh 100)
+                    ]
+                ]
+                [ Html.p
+                    [ css [ Css.property "grid-area" "debug" ] ]
+                    [ Html.text (Debug.toString model) ]
+                , Html.section
+                    [ css
+                        [ Css.property "grid-area" "notes"
+                        , Css.backgroundColor (Css.hex "EEEEEE")
+                        ]
+                    ]
+                    [ Html.button
+                        [ Events.onClick ClickedNewNote ]
+                        [ Html.text "New Note" ]
+                    , Database.notes model.database
+                        |> List.map
+                            (\node ->
+                                Html.li
+                                    -- TODO: make this accessible. Button?
+                                    [ Events.onClick (ClickedToSelectNote node.id) ]
+                                    [ Html.text node.content ]
+                            )
+                        |> Html.ul []
+                    ]
+                , Html.section
+                    [ css [ Css.property "grid-area" "content" ] ]
+                    [ case Maybe.andThen (\id -> Database.get id model.database) model.selected of
+                        Just node ->
+                            viewTree model.editing node model.database
+
+                        Nothing ->
+                            Html.text "Select a note on the left-hand side to get started."
+                    ]
                 ]
         ]
     }
+
+
+{-| LATER: this is the function to add lazy calls in!
+-}
+viewTree : Maybe Node.ID -> Node -> Database -> Html Msg
+viewTree editing node database =
+    Html.div []
+        [ if editing == Just node.id then
+            Html.form [ Events.onSubmit StoppedEditingNode ]
+                [ Html.input
+                    [ Attrs.value node.content
+                    , Events.onInput EditedNodeContents
+                    ]
+                    []
+                ]
+
+          else
+            -- TODO: how to make this accessible? A link?
+            Html.span [ Events.onClick (ClickedToEditNode node.id) ] [ Html.text node.content ]
+        ]
 
 
 main : Program () Model Msg
