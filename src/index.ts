e133052d0d72ed4f14a500b89d88816d2124c4d2
Brian Hicks <brian@brianthicks.com>
2020-04-20 17:20:50 -0500

    adjust indentation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
 // custom element
 class NoteInput extends HTMLTextAreaElement {
-    connectedCallback() {
-        console.log('connected');
-        console.log(this);
-    }
+  connectedCallback() {
+    console.log('connected');
+    console.log(this);
+    var self = this;
+    
+    document.addEventListener('selectionchange', function(event) {
+      // if (event.target.activeElement !== self) { return; }
+      const range = document.getSelection().getRangeAt(0);
+      console.log(range.getBoundingClientRect());
+      // self.dispatchEvent(new CustomEvent('select', { detail: {
+      //     start: {
+      //         node: range.startContainer,
+      //         offset: self.offsetUntil(range.startContainer) + range.startOffset
+      //     },
+      //     end: {
+      //         node: range.endContainer,
+      //         offset: self.offsetUntil(range.endContainer) + range.endOffset
+      //     },
+      //     originalEvent: range,
+      // }}));
+    });
+  }
 }
 
 customElements.define('note-input', NoteInput, { extends: 'textarea' });
