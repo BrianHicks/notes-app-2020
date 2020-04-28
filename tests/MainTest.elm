7cac768681ad4037b9948c92b61c1650ed88f537
Brian Hicks <brian@brianthicks.com>
2020-04-28 17:31:24 -0500

    test that I can see syntax errors

--- tests/MainTest.elm
+++ tests/MainTest.elm
@@ -70,6 +70,12 @@ programTest =
                     |> addNote (Database.idFromInt 0) "What's up?"
                     |> hitShortcutKey [] Escape
                     |> expectViewDoesntHaveInput
+        , test "if I make a syntax error, I should see it" <|
+            \_ ->
+                start
+                    |> clickButton "New Note"
+                    |> fillIn "content" "Content" "test ["
+                    |> expectNote (Query.find [ Selector.tag "li" ] >> Query.has [ Selector.text "While parsing a link, I was expecting the 'link' part of a [link](url)" ])
         , test "after editing a note, hitting enter creates a new child note" <|
             \_ ->
                 start
