08a41be63a90c07b0c1a86fe2647070a90943cce
Brian Hicks <brian@brianthicks.com>
2020-05-14 16:26:39 -0500

    don't minify for now

--- script/ninja.sh
+++ script/ninja.sh
@@ -9,16 +9,13 @@ elmFlags=--debug
 rule elm
   command = ./script/elm-make-module.sh \$in \$out \$elmFlags
 
-rule minify
-  command = uglifyjs \$in > \$out
-
 rule copy
   command = cp \$in \$out
 
 build dist/index.html: copy src/index.html
-build dist/script/custom-elements-builtin-bootstrap.js: minify src/custom-elements-builtin-bootstrap.js
-build dist/script/custom-elements-builtin.js: minify vendor/custom-elements-builtin.js
+build dist/script/custom-elements-builtin-bootstrap.js: copy src/custom-elements-builtin-bootstrap.js
+build dist/script/custom-elements-builtin.js: copy vendor/custom-elements-builtin.js
 build dist/script/elm.js: elm src/Main.elm | elm.json $(echo "$ELM_FILES" | tr '\n' ' ')
 build dist/script/index.js: copy src/index.js
-build dist/script/pouchdb.js: minify vendor/pouchdb-7.1.1.js
+build dist/script/pouchdb.js: copy vendor/pouchdb-7.1.1.js
 EOF
