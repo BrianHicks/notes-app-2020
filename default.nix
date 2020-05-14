48f4f4bd10da4646aea19db3cc22bbd97cb47e32
Brian Hicks <brian@brianthicks.com>
2020-05-14 16:45:17 -0500

    add nix packaging

---
+++ default.nix
@@ -0,0 +1,18 @@
+{ ... }:
+let
+  sources = import ./nix/sources.nix;
+  nixpkgs = import sources.nixpkgs { };
+  gitignore = import sources.gitignore { };
+in with nixpkgs;
+stdenv.mkDerivation {
+  name = "notes";
+  src = gitignore.gitignoreSource ./.;
+
+  buildInputs = [ elmPackages.elm ninja nodePackages.uglify-js ];
+  buildPhase = ''
+    ./script/ninja.sh > build.ninja
+    env ELM_HOME=.elm ninja
+  '';
+
+  installPhase = "cp -r dist/ $out/";
+}
